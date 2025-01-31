bbe6d2c4db1074b300c0533049c3344479f52324
Pranadeep Reddy Muppa <pranadeep.muppa@bimodalconsulting.com>
2025-01-31 12:20:48 -0600

    add AWSALBController module

---
+++ AWSALBController/variables.tf
@@ -0,0 +1,38 @@
+variable "oidc_url" {
+  type        = string
+  description = "OIDC issuer of the EKS cluster"
+}
+variable "namespace" {
+  type        = string
+  description = "Namespace to which Controller will be deployed"
+}
+variable "service_account" {
+  type        = string
+  description = "Service Account name associated to the controller pod"
+}
+variable "irsa_service_account" {
+  type        = list(string)
+  description = "Service Account with namespace format (['system:serviceaccount:namespace:service_account']) associated with IRSA"
+}
+variable "controller_version" {
+  type        = string
+  description = "Helm Chart version of the AWS ALB Controller"
+}
+variable "cluster_name" {
+  type        = string
+  description = "EKS Cluster Name"
+}
+
+variable "helm_values" {
+  type        = list(string)
+  description = "Helm Chart Values in yaml content"
+  default     = []
+}
+variable "app_name" {
+  description = "project name for which VPC is created"
+  type        = string
+}
+variable "environment" {
+  description = "environment name used in naming while creating resources"
+  type        = string
+}
